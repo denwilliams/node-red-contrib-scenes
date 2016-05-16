cb6ef5b7180824d426452d3d5e2ccc26d1634172
Den Williams <work@denwilliams.net>
2016-05-16 21:52:20 +1000

    travis publish

---
+++ scripts/travis-publish-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+./publish-docs.sh
