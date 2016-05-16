22c93a5dbf5a3cc28f75b0963e5f8eb873d9dcde
Den Williams <work@denwilliams.net>
2016-05-16 22:04:36 +1000

    try deploy script inside

--- scripts/travis-publish-docs.sh
+++ scripts/travis-publish-docs.sh
@@ -10,4 +10,16 @@ fi
 
 rev=$(git rev-parse --short HEAD)
 
-./scripts/publish-docs.sh
+echo "Deploying github pages for $rev"
+
+git stash
+npm run builddocs || exit 1
+git checkout gh-pages || exit 1
+mkdir doc
+# cp -R out/docs/ doc || exit 1
+mv out/docs/ doc/$(node -e "console.log(require('./package').version)") || exit 1
+git add doc/ || exit 1
+git commit -m "Generate documentation for v$(node -e "console.log(require('./package').version)")"
+git push origin gh-pages
+git checkout $branch
+git stash pop
